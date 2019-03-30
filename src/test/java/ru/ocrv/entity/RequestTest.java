d77460c6765dff8f190ca38532b6dde244455c71
Unknown <avesutok@gmail.com>
2019-03-30 22:51:11 +0300

    Coverage 83 perc

--- src/test/java/ru/ocrv/entity/RequestTest.java
+++ src/test/java/ru/ocrv/entity/RequestTest.java
@@ -1,11 +1,4 @@
 package ru.ocrv.entity;
 
-import org.junit.Test;
-
 public class RequestTest {
-    @Test
-    public void testCreateRequest(){
-        Request request = new Request("Some descr");
-        System.out.println(request);
-    }
 }
